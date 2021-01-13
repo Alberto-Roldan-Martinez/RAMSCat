e83b088fd121fa03f6f684a2d175f5a0dec442dc
alberto <alberto.roldan.martinez@googlemail.com>
2021-01-13 18:21:00 +0000

    13/01/2021

---
+++ Pre_Data_Collection/Plot_multi_MP_from_curve.py
@@ -0,0 +1,50 @@
+
+
+
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+site = "dist_" + str(sys.argv[1])                                         # site description, e.g. Mg
+if sys.argv[1] == "O":
+    x_min = 1.5
+    x_max = 5
+    y_min = -3
+    y_max = 0.1
+elif sys.argv[1] == "Mg":
+    x_min = 2
+    x_max = 5
+    y_min = -3
+    y_max = 0.1
+
+x = np.linspace(x_min, x_max, 101)
+figure = plt.figure(figsize=(12, 12), clear=True)
+
+for i in range(len(sys.argv)-2):
+    n = i + 2
+    curve = open(sys.argv[n], "r").readlines()[0].split()
+    d = float(curve[1])
+    a = float(curve[3])
+    r = float(curve[5])
+    y = []
+
+    for j in x:
+       y.append(d * (np.exp(-2 * a * (j - r)) - 2 * np.exp(-1 * a * (j - r))))
+
+    plt.plot(x, y, label=str(i+1))
+
+plt.xlabel("$dist_{" + str(sys.argv[1]) + "}$" +" ($\\AA$)", fontsize=14)
+plt.ylabel("$E_{Adh}$ (eV)", fontsize=14)
+#plt.xticks(np.arange(0,Xmax, 0.25))
+plt.xlim([x_min, x_max])#[min(x)*0.9, max(x)*1.2])
+plt.ylim([y_min, y_max]) #[min(y)*1.2, 0.1])
+#plt.tick_params(axis='both', labelrotation=0, labelsize=16)               # custimise tick labels
+#plt.grid(True)
+plt.legend(loc='best')
+#plt.title("$TM_{" + str(int(data[0,0])) + "}$ $dist_{" + str(sys.argv[2]) + "}$")
+#plt.savefig("MP" + str(int(data[0,0])) + "_" + site +".png", figsize=(11.69, 16.53), clear=True, dpi=300, orientation='landscape', transparent=True)
+plt.show()
+
+
