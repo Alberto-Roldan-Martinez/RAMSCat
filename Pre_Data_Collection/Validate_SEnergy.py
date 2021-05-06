1e8925131d68b792da2eb58b3effd154eb798510
alberto <alberto.roldan.martinez@googlemail.com>
2021-05-06 16:19:40 +0100

    06/05/2021

--- Pre_Data_Collection/Validate_SEnergy.py
+++ Pre_Data_Collection/Validate_SEnergy.py
@@ -37,28 +37,29 @@ def SaveFig():
                 plt.savefig(figure_out_name + ".svg", figsize=(12, 10), clear=True,
                                         bbox_inches='tight', dpi=300, orientation='landscape', transparent=True)
 
-def annotation(note, x0, y0, x, y):
+def annotation(note, arrow_site, x0, y0, x, y):
     return plt.annotate(note, xy=(x0,y0), xycoords="data", size=14,
                                xytext=(x,y), textcoords="data",
                                arrowprops=dict(arrowstyle="<-", color="k", fc="0.6"),
-                               horizontalalignment="center", verticalalignment="center")
+                               horizontalalignment=arrow_site, verticalalignment="center")
 
 
 def notes(iline):
     line = iline[-1].split("/")
-    surface = line[0]
-    size = line[1].split("_")
-    if len(size) > 1:
-        if "v" in size[1]:
-            defect = list(size[1])[1]
-            inote = str("("+surface+")$ \cdot \it p($"+size[0]+"$) \minus $"+defect)
-        elif "a" in size[1]:
-            defect = list(size[1])[-1]
-            inote = str("("+str(surface)+")$ \cdot \it p($"+str(size[0])+"$) \plus $"+str(defect))
+    if line[0].startswith("#") is True:
+        inote = "no"
     else:
-        inote = str("("+str(surface)+")$ \cdot \it p("+str(size[0])+")$")
-
-    print(inote, len(inote))
+        surface = line[0]
+        size = line[1].split("_")
+        if len(size) > 1:
+            if "v" in size[1]:
+                defect = list(size[1])[1]
+                inote = str("("+surface+")$ \cdot \it p($"+size[0]+"$) \minus $"+defect)
+            elif "a" in size[1]:
+                defect = list(size[1])[-1]
+                inote = str("("+str(surface)+")$ \cdot \it p($"+str(size[0])+"$) \plus $"+str(defect))
+        else:
+            inote = str("("+str(surface)+")$ \cdot \it p("+str(size[0])+")$")
 
     return inote
 
@@ -78,38 +79,20 @@ def Area(file_name, coord_areas):
         surf_coord = [int(iline[j]) for j in range(2, len(iline)-2)]
         y.append(np.dot(coord_areas, surf_coord))                    # in Angstroms^2
 
-    print(x, y)
-    x_min = min(x)-50
-    x_max = max(x)+100
+    x_min = min(x)*0.9
+    x_max = max(x)*1.15
     plt.plot([x_min, x_max], [x_min, x_max], "k-", lw=1.5)
-    plt.plot(x, y, "rs")
-
-    i = 0  ;   annotation(note[i], x[i], y[i], x[i]+50, y[i]-60)
-    i = 1  ;   annotation(note[i], x[i], y[i], x[i]+300, y[i]-55)
-    i = 2  ;   annotation(note[i], x[i], y[i], x[i]+300, y[i]+0)
-    i = 3  ;   annotation(note[i], x[i], y[i], x[i]+300, y[i]-75)
-    i = 4  ;   annotation(note[i], x[i], y[i], x[i]+310, y[i]-35)
-    i = 5  ;   annotation(note[i], x[i], y[i], x[i]+300, y[i]+40)
-    i = 6  ;   annotation(note[i], x[i], y[i], x[i]-80, y[i]+5)
-    i = 7  ;   annotation(note[i], x[i], y[i], x[i]+180, y[i]-0)
-    i = 8  ;   annotation(note[i], x[i], y[i], x[i]-100, y[i]+45)
-#    i = 9  ;   annotation(note[i], x[i], y[i], x[i]-80, y[i]+15)
-#    i = 10 ;   annotation(note[i], x[i], y[i], x[i]-70, y[i]+25)
-#    i = 11 ;   annotation(note[i], x[i], y[i], x[i]-70, y[i]+65)
-#    i = 12 ;   annotation(note[i], x[i], y[i], x[i]-50, y[i]+40)
-#    i = 13 ;   annotation(note[i], x[i], y[i], x[i]+130, y[i]-40)
-#    i = 14 ;   annotation(note[i], x[i], y[i], x[i]+300, y[i]+17)
-#    i = 15 ;   annotation(note[i], x[i], y[i], x[i]+15, y[i]+40)
-#    i = 16 ;   annotation(note[i], x[i], y[i], x[i]+300, y[i]-20)
-#    i = 17 ;   annotation(note[i], x[i], y[i], x[i]+180, y[i]+20)
-#    i = 18 ;   annotation(note[i], x[i], y[i], x[i]-60, y[i]+80)
-#    i = 19 ;   annotation(note[i], x[i], y[i], x[i]+150, y[i]+25)
-#    i = 20 ;   annotation(note[i], x[i], y[i], x[i]+250, y[i]+18)
-#    i = 21 ;   annotation(note[i], x[i], y[i], x[i]+250, y[i]+40)
-#    i = 22 ;   annotation(note[i], x[i], y[i], x[i]-200, y[i]-20)
-#    i = 23 ;   annotation(note[i], x[i], y[i], x[i]-200, y[i]-0)
-#    i = 24 ;   annotation(note[i], x[i], y[i], x[i]+10, y[i]+50)
-
+    plt.plot(x, y, "rs", ms=3)
+    for i in range(len(x)):
+        if note[i] != "no":
+            if y[i] <= x_min + (x_max - x_min)/2:
+                x_margin = x_max - max([len(note[i]) for i in range(len(note))])
+                y_margin = y[i]
+                annotation(note[i], "left", x[i], y[i], x_margin, y_margin)
+            else:
+                x_margin = x_min + max([len(note[i]) for i in range(len(note))])
+                y_margin = y[i]
+                annotation(note[i], "right", x[i], y[i], x_margin, y_margin)
     Display("Area ($\\AA ^{2}$)", "Predicted Area ($\\AA ^{2}$)", [x_min, x_max], [x_min, x_max], "")
 
 ########################################################################
@@ -127,38 +110,20 @@ def Surf_E(file_name, coord_surf_e):
         surf_coord = [int(iline[j]) for j in range(2, len(iline)-2)]
         y.append(np.dot(coord_surf_e, surf_coord))                    # in Angstroms^2
 
-    print(x, y)
-    x_min = min(x)-np.abs(min(x))*0.15
+    x_min = min(x)*0.9
     x_max = max(x)*1.15
     plt.plot([x_min, x_max], [x_min, x_max], "k-", lw=1.5)
-    plt.plot(x, y, "bo")
-
-    i = 0  ;   annotation(note[i], x[i], y[i], x[i]+0.35, y[i]+0.008)
-    i = 1  ;   annotation(note[i], x[i], y[i], x[i]+0.15, y[i]-0.14)
-    i = 2  ;   annotation(note[i], x[i], y[i], x[i]+0.25, y[i]-0.11)
-    i = 3  ;   annotation(note[i], x[i], y[i], x[i]+0.30, y[i]+0.08)
-    i = 4  ;   annotation(note[i], x[i], y[i], x[i]+0.35, y[i]+0.035)
-    i = 5  ;   annotation(note[i], x[i], y[i], x[i]+0.32, y[i]-0.075)
-    i = 6  ;   annotation(note[i], x[i], y[i], x[i]+0.35, y[i]-0.03)
-    i = 7  ;   annotation(note[i], x[i], y[i], x[i]+0.30, y[i]-0.10)
-    i = 8  ;   annotation(note[i], x[i], y[i], x[i]+0.23, y[i]-0.13)
-#    i = 9  ;   annotation(note[i], x[i], y[i], x[i]+0.35, y[i]+0.07)
-#    i = 10 ;   annotation(note[i], x[i], y[i], x[i]-0.14, y[i]+0.07)
-#    i = 11 ;   annotation(note[i], x[i], y[i], x[i]+0.35, y[i]-0.05)
-#    i = 12 ;   annotation(note[i], x[i], y[i], x[i]-0.12, y[i]+0.05)
-#    i = 13 ;   annotation(note[i], x[i], y[i], x[i]-0.30, y[i])
-#    i = 14 ;   annotation(note[i], x[i], y[i], x[i]-0.02, y[i]+0.12)
-#    i = 15 ;   annotation(str(i) + note[i], x[i], y[i], x[i]-0.20, y[i]-0.05)
-#    i = 16 ;   annotation(str(i) + note[i], x[i], y[i], x[i], y[i])
-#    i = 17 ;   annotation(note[i], x[i], y[i], x[i]-0.30, y[i]-0.05)
-#    i = 18 ;   annotation(note[i], x[i], y[i], x[i]-0.20, y[i]+0.00)
-#    i = 19 ;   annotation(note[i], x[i], y[i], x[i]+0.18, y[i]+0.00)
-#    i = 20 ;   annotation(str(i) + note[i], x[i], y[i], x[i], y[i])
-#    i = 21 ;   annotation(str(i) + note[i], x[i], y[i], x[i], y[i])
-#    i = 22 ;   annotation(str(i) + note[i], x[i], y[i], x[i], y[i])
-#    i = 23 ;   annotation(str(i) + note[i], x[i], y[i], x[i], y[i])
-#    i = 24 ;   annotation(note[i], x[i], y[i], x[i]-0.10, y[i]+0.08)
-
+    plt.plot(x, y, "bo", ms=3)
+    for i in range(len(x)):
+        if note[i] != "no":
+            if y[i] <= x_min + (x_max - x_min)/2:
+                x_margin = x_max - (x_max - x_min) * max([len(note[i]) for i in range(len(note))]) / 110
+                y_margin = y[i]
+                annotation(note[i], "left", x[i], y[i], x_margin, y_margin)
+            else:
+                x_margin = x_min + (x_max - x_min) * max([len(note[i]) for i in range(len(note))]) / 110
+                y_margin = y[i]
+                annotation(note[i], "right", x[i], y[i], x_margin, y_margin)
     Display("$\\gamma$ ($J \cdot m^{\minus 2}$)", "Predicted $\\gamma$ ($J \cdot m^{\minus 2}$)", [x_min, x_max],
             [x_min, x_max], "")
 
