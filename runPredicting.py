ff316920fcc3db5e8c51bbbe0f6b4e4792956dca
alberto <alberto.roldan.martinez@googlemail.com>
2021-10-08 09:03:42 +0100

    08/10/2021

--- runPredicting.py
+++ runPredicting.py
@@ -5,6 +5,7 @@ Please cite - A. Shayeghi et al, PCCP, 2015, 17, 2104-2112
 Authors - The Johnston Group 10/7/15
 
 """
+import os
 import time
 import GA.Input as In
 #from GA.Graphene import Graphene                        # to generate the surface
@@ -17,8 +18,14 @@ eleNames = ["Au"]                                       # elements in the cluste
 eleNums = [10]                                           # number of atoms in the cluster
 boxAdd = 15.0
 nPool = 15                                              # number of structures in the pool file
-cross = "random"                                        # algorithm to generate structures
-mutType = "move"                                        # algorithm to generate mutants
+# --- algorithm to generate structures
+cross = "random"                                        #
+#cross = "weighted"										# determined by fitness of the two clusters selected for crossover.
+# --- algorithm to generate mutants
+#mutType = "random"										# new random cluster geometry
+mutType = "move"										# selected from the pool and 20% of the geometry is displaced by up to 1 angstrom
+#mutType = "homotop"									# (only bimetallics) two atoms have their atom types swapped
+#mutType = "rotate"										# (Surface global optimisation only) selected from the pool and a random rotation is performed.
 mutate = 0.1                                            # mutation ratio
 r_ij = 2.8                                              # distance between atoms in the cluster? e.g. 3rd row -> 2.5
 eleMasses = In.masses(eleNames)
@@ -26,7 +33,7 @@ natoms = sum(eleNums)
 
 surfGA = True                                           # is it a supported cluster?
 #surface = Graphene(x=8,y=8,z=1,vac=15,clusHeight=2.5)  # how is the support's surface
-surface = MgO(x=8, y=8, z=2, vac=6, clusHeight=2.3)     # how is the support's surface
+surface = MgO(x=8, y=8, z=4, vac=6, clusHeight=2.3)     # how is the support's surface
 
 subString = "/home/alberto/Software/OTHER/NeuralNetwork/Predicting.py"     # package to calculate the Energy
 
