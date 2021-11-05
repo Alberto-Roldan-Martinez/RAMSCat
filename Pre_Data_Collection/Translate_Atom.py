70c76dc33e9ab75c53441a266cf633a1cb635df3
alberto <alberto.roldan.martinez@googlemail.com>
2021-11-05 09:28:28 +0000

    05/11/2021

--- Pre_Data_Collection/Translate_Atom.py
+++ Pre_Data_Collection/Translate_Atom.py
@@ -10,7 +10,7 @@ import sys
 from ase.io import read, write
 
 
-displacements = [-0.2, -0.1, -0.05, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 1.5, 2, 3, 5]		# list of displacements along Z
+displacements = [-0.5, -0.3, -0.2, -0.1, -0.05, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 1.5, 2, 3, 5]		# list of displacements along Z
 structure_file = sys.argv[1] 		# file with the structure
 atom_index = int(sys.argv[2])			# atom index to translate
 try:
