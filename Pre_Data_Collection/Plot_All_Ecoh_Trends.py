01ccd699ead9c5c3bf3cd06f015c791456c3ae13
alberto <alberto.roldan.martinez@googlemail.com>
2022-04-04 16:25:25 +0100

    04/04/2022

--- Pre_Data_Collection/Plot_All_Ecoh_Trends.py
+++ Pre_Data_Collection/Plot_All_Ecoh_Trends.py
@@ -7,6 +7,9 @@
 
 import sys
 import numpy as np
+import numpy.ma as ma
+import scipy as sp
+import scipy.interpolate
 from scipy.optimize import curve_fit
 import matplotlib as mpl
 mpl.use('TkAgg')
@@ -48,17 +51,33 @@ def Display_2axis(labels, x_label, x, y1, y2):
 
 
 def Display3D(labels, x1, x2, y, y_label):
+	def plane(x, z, a, b, c, d, e):
+		return a*np.exp(-b*x[0]) + c*np.exp(-d*x[1]) + e
+
+	popt, pcov = curve_fit(plane, [x1, x2], y)#, bounds=limits)
+	r2 = 1-np.sqrt(sum([(y[i] - plane([x1[i], x2[i]], *popt))**2 for i in range(len(y))])/sum(i*i for i in y))
+	print(popt, "\n", r2)
+
 	figure = plt.figure(figsize=(10, 10), clear=True)		# prepares a figure
 	ax = figure.add_subplot(111, projection='3d') 			#plt.axes(projection='3d')
-	ax.scatter3D(x1, x2, y1, s=5, c='k', marker='o')
+	ax.scatter3D(x1, x2, y1, s=5, c='k', marker='o', label="$R^{2}=$ " + str(round(r2, 2)))
+	z_lim = [min(y), max(y)]
+#	spline = sp.interpolate.Rbf(x1, x2, y, function='thin_plate', smooth=5, episilon=1)
 
 	grid = 50
 	surf_x = np.linspace(0, max(x1)*1.1, grid)
 	surf_y = np.linspace(0, max(x2)*1.1, grid)
-#	x, y = np.meshgrid(surf_x, surf_y)
-#	z = generalised_morse_3D([x, y], *popt)			# popt = y_max, a1, a2, a3, a4, d1, d2, r1, r2, m
+	x, y = np.meshgrid(surf_x, surf_y)
+#	z = spline(x, y)
+	z = plane([x, y], *popt)
 
-#	surface = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis', alpha=0.7, vmin=zlim[0], vmax=0)
+# masking the data beyond zmax
+	z_mask_max = ma.masked_greater_equal(z, 0.0, copy=True)
+	z = z_mask_max.filled(fill_value=0.0)
+	z_mask_min = ma.masked_less(z, z_lim[0], copy=True)
+	z = z_mask_min.filled(fill_value=z_lim[0])
+
+	surface = ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='viridis', alpha=0.7, vmin=z_lim[0], vmax=0)
 #	figure.colorbar(surface, shrink=0.25, aspect=10)
 
 	ax.set_xlabel('coordination', fontsize=16)
@@ -66,18 +85,18 @@ def Display3D(labels, x1, x2, y, y_label):
 	ax.set_zlabel(y_label, fontsize=16, labelpad=10)
 	ax.set_xlim3d(0, max(x1)*1.1)
 	ax.set_ylim3d(0, max(x2)*1.1)
-	ax.set_zlim3d(min(z)-0.1, max(z)+0.1)
+	ax.set_zlim3d(z_lim)
 	ax.tick_params(axis='both', labelsize=14)
 #	ax.set_xticks([])
 #	ax.set_yticks([])
 #	ax.set_zticks(np.linspace(0, 1, 5))
 #	plt.subplots_adjust(left=0.15, right=0.9, top=0.8, bottom=0.1)
 #	legend = ax1.legend(bbox_to_anchor=(0.5, 1.05), loc='upper center')
-#	legend = ax.legend(loc="best")
+	legend = ax.legend(loc="best")
 	figure.tight_layout()
 	plt.grid(True)
 	plt.ion()
-	ax.view_init(azim=-120, elev=10)
+	ax.view_init(azim=45, elev=10)
 	plt.show()
 	SaveFig()
 	plt.clf()
@@ -100,7 +119,7 @@ x2 = [float(data[i][2]) for i in range(len(data))]
 y1 = [float(data[i][3]) for i in range(len(data))]
 y2 = [float(data[i][4]) for i in range(len(data))]
 
-Display_2axis(label, 'coordination', x1, y1, y2)
-Display_2axis(label, 'GCN', x2, y1, y2)
+#Display_2axis(label, 'coordination', x1, y1, y2)
+#Display_2axis(label, 'GCN', x2, y1, y2)
 
 Display3D(label, x1, x2, y1, '$E_{eq}$ $(eV \cdot atom^{\minus 1})$')
